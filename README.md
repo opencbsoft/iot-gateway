3e2917a39b6215415b075fb74a81e0f94e7d2471
Cristian Boboc <cristi@pidginhost.com>
2015-09-27 11:58:07 +0300

    Initial project data

---
+++ README.md
@@ -0,0 +1,19 @@
+# Information
+This is the project that should act as an I.O.T. Gateway, allowing all devices to communicate and to provide meaning
+
+The clients can be of 3 types
+- 0 READ (temperature, humidity, motion) 
+- 1 WRITE (Relay, door lock, remote)
+- 2 READ/WRITE (TouchScreen)
+
+# Url calling
+ 
+ /type/?value=n
+
+# READ clients
+
+# WRITE clients
+
+# READ/WRITE clients
+
+
---
+++ core/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+
+from core.models import Device
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+def index(request):
+    data = dict()
+    data['ip'] = get_client_ip(request)
+    data['devices'] = Device.objects.all()
+    if request.method == 'POST':
+        if request.POST.get('action') == 'disable':
+            dev = Device.objects.get(id=request.POST.get('id'))
+            dev.enabled = False
+            dev.save()
+            return redirect('index')
+        elif request.POST.get('action') == 'enable':
+            dev = Device.objects.get(id=request.POST.get('id'))
+            dev.enabled = True
+            dev.save()
+            return redirect('index')
+    return render(request, 'index.html', data)
+
+
+def device(request, url):
+    data = dict()
+    ip = get_client_ip(request)
+    if url not in ['0', '1', '2']:
+        data['error'] = 'The type of the client should be one of these "0" "1" "2".'
+    else:
+        dev, dev_created = Device.objects.get_or_create(ip=get_client_ip(request), type=url)
+        """
+            PROCESS DEVICE LOGIC
+        """
+
+        data['device'] = dev
+    return render(request, 'device.html', data)
