53774c53182e2baa57fa5c51bedd1eaa57787ab4
Cristian Boboc <cristi@pidginhost.com>
2015-10-05 17:27:11 +0300

    added test_workers file

--- workers/speak_worker.py
+++ workers/speak_worker.py
@@ -29,12 +29,16 @@ DEFAULT_SOUND_VOLUME = 80
 
 def on_message(client, user_data, msg):
     data = json.loads(msg.payload)
+    f = open('myfile','w')
+    f.write(str(data)) # python will convert \n to os.linesep
+    f.close() # you can omit in most cases as the destructor will call it
     if data.get('left', False):
         call(['amixer', "-q", 'sset', 'Speaker', '0%,{0}%'.format(DEFAULT_SOUND_VOLUME)])
     elif data.get('right', False):
         call(['amixer', "-q", 'sset', 'Speaker', '{0}%,0%'.format(DEFAULT_SOUND_VOLUME)])
     call(["mpg123", "-q", "http://translate.google.com/translate_tts?tl=%s&q=%s&ie=%s&total=1&idx=0&client=t" % ("en", data.get('message', 'No message was set'), "UTF-8")])
     call(['amixer', "-q", 'sset', 'Speaker', '{0}%,{0}%'.format(DEFAULT_SOUND_VOLUME)])
+
 
 
 #create a daemon and run the worker
