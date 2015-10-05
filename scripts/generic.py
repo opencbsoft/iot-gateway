2576a43c26343dcaebf92348ebb5144934f4f760
Cristian Boboc <cristi@pidginhost.com>
2015-10-05 17:45:53 +0300

    added test_workers file

--- scripts/generic.py
+++ scripts/generic.py
@@ -1,5 +1,11 @@
+import sys
+
 from workers.generic_worker import iot_publish
 
 
-iot_publish('play_sound', {'file':'ceva.mp3'})
-iot_publish('speak', {'message':'Salut'})
+def main(data):
+    # 0 means it is successfull
+    #iot_publish('play_sound', {'file':'ceva.mp3'})
+    iot_publish('speak', {'message':'The temperature is '+str(data.get('temperature', 25))+' degrees'})
+    return 0
+
