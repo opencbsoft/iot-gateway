38d5c4dd7e49336b2b73a29e2f046e8b0483b529
Cristian Boboc <cristi@pidginhost.com>
2015-10-05 12:44:42 +0300

    updated

--- workers/ioteo_worker.py
+++ workers/ioteo_worker.py
@@ -50,15 +50,15 @@ if __name__ == "__main__":
         daemon = IotDaemon('/tmp/daemon-iot.pid')
         if len(sys.argv) == 2:
             if 'start' == sys.argv[1]:
-                    daemon.start()
+                daemon.start()
             elif 'stop' == sys.argv[1]:
-                    daemon.stop()
+                daemon.stop()
             elif 'restart' == sys.argv[1]:
-                    daemon.restart()
+                daemon.restart()
             else:
-                    print "Unknown command"
-                    sys.exit(2)
+                print ("Unknown command")
+                sys.exit(2)
             sys.exit(0)
         else:
-            print "usage: %s start|stop|restart" % sys.argv[0]
+            print ("usage: %s start|stop|restart" % sys.argv[0])
             sys.exit(2)
