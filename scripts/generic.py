f5e8c473457f8361789a5be05503ee1f8b5b5051
Cristian Boboc <cristi@pidginhost.com>
2015-10-26 14:50:05 +0200

    update

--- scripts/generic.py
+++ scripts/generic.py
@@ -1,12 +1,10 @@
 import sys
-import urllib
-
 from workers.generic_worker import iot_publish
 
 
 def main(data):
     # 0 means it is successfull
     #iot_publish('play_sound', {'file':'ceva.mp3'})
-    iot_publish('speak', {'message':urllib.urlencode('The temperature is '+str(data.get('temperature', 25))+' degrees')})
+    iot_publish('speak', {'message': 'The temperature is '+str(data.get('temperature', 25))+' degrees'})
     return 0
 
